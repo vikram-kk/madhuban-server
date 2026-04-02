2fd1a4c94c0805eb53d7d008a46f6ab892815b99
Vikram Thakur <vikramkk380@gmail.com>
2026-04-02 23:42:46 +0530

    register controller check

--- models/User.model.js
+++ models/User.model.js
@@ -12,7 +12,7 @@ const userSchema = new mongoose.Schema({
         lowercase: true,
         trim: true
     },
-    phone_number: {
+    phone: {
         type: String,
         required: true
     },
