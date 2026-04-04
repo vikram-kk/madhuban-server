ff42204a6fc34bfb63f156654e09ccba175480a4
Vikram Thakur <vikramkk380@gmail.com>
2026-04-04 22:36:52 +0530

    product route setup

--- routes/product.route.js
+++ routes/product.route.js
@@ -6,5 +6,11 @@ import { roleMid } from '../middlewares/role.middleware.js'
 const router = express.Router()
 
 
+router.post("/createProduct", authMid, roleMid('admin'), createProduct)
+router.get("/products", getProducts)
+router.get("/products/:id", getProductById)
+router.get("/products/update/:id", authMid, roleMid('admin'), updateProduct)
+router.delete("/products/delete/:id", authMid, roleMid('admin'), getProductById)
+
 
 export default router
