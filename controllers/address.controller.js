da652d9ab39bb4a12c09f46880bd4b55759c3ef8
Vikram Thakur <vikramkk380@gmail.com>
2026-06-05 23:55:34 +0530

    add address controller

---
+++ controllers/address.controller.js
@@ -0,0 +1,110 @@
+import Address from "../models/Address.model"
+
+export const userAddresses = async (req, res) => {
+    try {
+        let locations = []
+        const userId = req.user._id
+        locations = await Address.find({ user: userId })
+        if (locations.length == 0) {
+            return res.status(404).json({
+                message: "No address saved",
+                success: false
+            })
+        }
+        res.status(200).json({
+            message: 'addresses found',
+            success: true,
+            locations
+        }
+        )
+
+    } catch (error) {
+        res.status(500).json({
+            message: `error at ${error.message}`,
+            success: false
+        })
+    }
+}
+
+export const createAddress = async (req, res) => {
+    try {
+        //getting address in address from object 
+        const { addressForm } = req.body;
+        const userId = req.user._id;
+        // validating the address form 
+        if (!addressForm) {
+            return res.status(400).json({
+                message: "Address data is required",
+                success: false
+            });
+        }
+
+        //validating all the feild in the adress form
+        const {
+            fullName,
+            phone,
+            street,
+            city,
+            state,
+            pincode
+        } = addressForm;
+
+        if (
+            !fullName ||
+            !phone ||
+            !street ||
+            !city ||
+            !state ||
+            !pincode
+        ) {
+            return res.status(400).json({
+                message: "All fields are required",
+                success: false
+            });
+        }
+        // creating new address 
+        const newAddress = await Address.create({
+            user: userId,
+            ...addressForm
+        });
+
+        return res.status(201).json({
+            message: "Address saved successfully",
+            success: true,
+            address: newAddress
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            message: `Internal server error: ${error.message}`,
+            success: false
+        });
+    }
+};
+
+export const deleteAddress = async (req, res) => {
+    try {
+        // accessing the address using the address Id
+        const addressId = req.params.addressId
+        //validating the address
+        if (!addressId) {
+            return res.status(404).json({
+                message: `choose valid address`,
+                success: false
+            })
+        }
+
+        // delete the address accessing the Address directly
+        const address = await Address.deleteOne({ _id: addressId })
+        res.status(200).json({
+            message: `address removed successfully`,
+            success: true
+        })
+
+    } catch (error) {
+        return res.status(500).json({
+            message: `Internal server error in delete address : ${error.message}`,
+            success: false
+        });
+    }
+}
